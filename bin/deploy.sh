214a38ba3c0e931f6215c66cf96f5d58dd43f27a
Mat Gargano <mgargano@forthepeople.com>
2015-11-09 10:05:05 -0500

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+function cleanUpDirectories {
+    rm -rf $CANONICAL_HOLDING_CELL $PANTHEON_HOLDING_CELL $INTERIM_HOLDING_CELL
+}
 
 DATE=`date +%Y%m%d%H%M%S`
 CANONICAL_REPO="git@github.com:matgargano/local-test-site.git"
@@ -13,13 +15,15 @@ INTERIM_HOLDING_CELL="$HOME/temp/interim-holding-cell"
 INTERIM_PLUGINS_DIR="$INTERIM_HOLDING_CELL/wp-content/plugins"
 INTERIM_THEMES_DIR="$INTERIM_HOLDING_CELL/wp-content/themes"
 
-PANTHEON_REPO="git clone ssh://codeserver.dev.f4e63549-adc4-43f0-bdbf-c5367d853e83@codeserver.dev.f4e63549-adc4-43f0-bdbf-c5367d853e83.drush.in:2222/~/repository.git"
+PANTHEON_REPO="ssh://codeserver.dev.f4e63549-adc4-43f0-bdbf-c5367d853e83@codeserver.dev.f4e63549-adc4-43f0-bdbf-c5367d853e83.drush.in:2222/~/repository.git
 PANTHEON_HOLDING_CELL="$HOME/temp/pantheon-holding-cell"
 
 
 
+# set up a blank slate
+cleanUpDirectories
 
-rm -rf $CANONICAL_HOLDING_CELL $PANTHEON_HOLDING_CELL $INTERIM_HOLDING_CELL
+
 mkdir -p $CANONICAL_HOLDING_CELL
 mkdir -p $PANTHEON_HOLDING_CELL
 mkdir -p $INTERIM_HOLDING_CELL
@@ -32,16 +36,21 @@ git clone $INTERIM_REPO $INTERIM_HOLDING_CELL
 git clone $PANTHEON_REPO $PANTHEON_HOLDING_CELL
 
 
+
+
 #add pantheons updates to core
 cd $INTERIM_HOLDING_CELL
 rm -rf *
 cp -R $PANTHEON_HOLDING_CELL/* .
+git add .
 git commit -am "adding in pantheon repo for $DATE deploy"
 
+
+
 # add our canonical plugins and themes
-rm -rf $INTERIM_PLUGINS_DIR $INTERIM_THEMES_DIR
-cp -R $CANONICAL_PLUGINS_DIR $INTERIM_PLUGINS_DIR
-cp -R $CANONICAL_THEMES_DIR $INTERIM_THEMES_DIR
+ rm -rf $INTERIM_PLUGINS_DIR $INTERIM_THEMES_DIR
+ cp -R $CANONICAL_PLUGINS_DIR $INTERIM_PLUGINS_DIR
+ cp -R $CANONICAL_THEMES_DIR $INTERIM_THEMES_DIR
 
 cd $INTERIM_HOLDING_CELL
 git add .
@@ -52,3 +61,6 @@ git push origin head
 git push origin --tags
 git remote add pantheon $PANTHEON_REPO
 git push pantheon master -f
+
+# clean up
+# cleanUpDirectories
