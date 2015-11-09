6fa9c040341d432e5ba7e0378f7521ead8bcd475
Mat Gargano <mgargano@forthepeople.com>
2015-11-09 08:49:45 -0500

    updates

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,67 +1,46 @@
 #!/bin/bash
 
-GIT_REPO="clone git@github.com:matgargano/testpantheon.git"
-PANTHEON_REPO="ssh://codeserver.dev.f01647d8-b334-42b6-a823-ba7d1c9ae609@codeserver.dev.f01647d8-b334-42b6-a823-ba7d1c9ae609.drush.in:2222/~/repository.git"
+
+
 DATE=`date +%Y%m%d%H%M%S`
-HOLDING_CELL="/Users/morgandeveloper/Sites/holding-cell"
-PANTHEON_HOLDING_CELL="/Users/morgandeveloper/Sites/pantheon-holding-cell"
+CANONICAL_REPO="git@github.com:matgargano/local-test-site.git"
+CANONICAL_HOLDING_CELL="/Users/morgandeveloper/temp/canonical-holding-cell"
+CANONICAL_PLUGINS_DIR="$CANONICAL_HOLDING_CELL/web/app/plugins"
+CANONICAL_THEMES_DIR="$CANONICAL_HOLDING_CELL/web/app/themes"
 
-rm -rf $HOLDING_CELL
-rm -rf $PANTHEON_HOLDING_CELL
-mkdir -p $HOLDING_CELL
+INTERIM_REPO="git@github.com:matgargano/interim-pantheon-wp.git"
+INTERIM_HOLDING_CELL="/Users/morgandeveloper/temp/interim-holding-cell"
+INTERIM_PLUGINS_DIR="$INTERIM_HOLDING_CELL/wp-content/plugins"
+INTERIM_THEMES_DIR="$INTERIM_HOLDING_CELL/wp-content/themes"
+
+PANTHEON_REPO="ssh://codeserver.dev.d68a4e73-f922-4258-94ff-84749fee08f1@codeserver.dev.d68a4e73-f922-4258-94ff-84749fee08f1.drush.in:2222/~/repository.git"
+PANTHEON_HOLDING_CELL="/Users/morgandeveloper/temp/pantheon-holding-cell"
+PANTHEON_PLUGINS_DIR="$PANTHEON_HOLDING_CELL/wp-content/plugins"
+PANTHEON_THEMES_DIR="$PANTHEON_HOLDING_CELL/wp-content/themes"
+
+
+
+rm -rf $CANONICAL_HOLDING_CELL $PANTHEON_HOLDING_CELL $INTERIM_HOLDING_CELL
+mkdir -p $CANONICAL_HOLDING_CELL
 mkdir -p $PANTHEON_HOLDING_CELL
+mkdir -p $INTERIM_HOLDING_CELL
 
-git $GIT_REPO $HOLDING_CELL
+git clone $CANONICAL_REPO $CANONICAL_HOLDING_CELL
+cd $CANONICAL_HOLDING_CELL
+composer install
 
-cd $HOLDING_CELL
-rm -rf wp-content/plugins
-rm -rf wp-content/themes
-rm -rf wp-content/mu-plugins
-if [ -d /Users/morgandeveloper/Sites/pantheon-test/web/app/themes ]; then
-  cp -R /Users/morgandeveloper/Sites/pantheon-test/web/app/themes $HOLDING_CELL/wp-content/themes
-fi
-
-if [ -d /Users/morgandeveloper/Sites/pantheon-test/web/app/plugins ]; then
-  cp -R /Users/morgandeveloper/Sites/pantheon-test/web/app/plugins $HOLDING_CELL/wp-content/plugins
-fi
-
-if [ -d /Users/morgandeveloper/Sites/pantheon-test/web/app/mu-plugins ]; then
-  cp -R /Users/morgandeveloper/Sites/pantheon-test/web/app/mu-plugins $HOLDING_CELL/wp-content/mu-plugins
-fi
-cd $HOLDING_CELL
-git pull --no-rebase --squash -Xtheirs $PANTHEON_REPO master
-git add -A
-git commit -m "Syncing with Pantheon core files"
-
-git tag -a $DATE -m "tagging release $DATE"
-
-git push origin head
-git push --tags
-
-
+git clone $INTERIM_REPO $INTERIM_HOLDING_CELL
 git clone $PANTHEON_REPO $PANTHEON_HOLDING_CELL
 
-rm -rf $PANTHEON_HOLDING_CELL/wp-content/plugins
-rm -rf $PANTHEON_HOLDING_CELL/wp-content/themes
-rm -rf $PANTHEON_HOLDING_CELL/wp-content/mu-plugins
 
+rm -rf $INTERIM_PLUGINS_DIR $INTERIM_THEMES_DIR
+cp -R $CANONICAL_PLUGINS_DIR $INTERIM_PLUGINS_DIR
+cp -R $CANONICAL_THEMES_DIR $INTERIM_THEMES_DIR
 
-if [ -d $HOLDING_CELL/wp-content/themes ]; then
-  cp -R $HOLDING_CELL/wp-content/themes $PANTHEON_HOLDING_CELL/wp-content/themes
-fi
-
-if [ -d $HOLDING_CELL/wp-content/plugins ]; then
-  cp -R $HOLDING_CELL/wp-content/plugins $PANTHEON_HOLDING_CELL/wp-content/plugins
-fi
-
-if [ -d $HOLDING_CELL/wp-content/mu-plugins ]; then
-  cp -R $HOLDING_CELL/wp-content/mu-plugins $PANTHEON_HOLDING_CELL/wp-content/mu-plugins
-fi
-
-cd $PANTHEON_HOLDING_CELL
-
-git add -A
-git commit -am "Pushing up $DATE"
-git merge --strategy-option ours
+cd $INTERIM_HOLDING_CELL
+git add .
+git commit -am "COMMIT $DATE"
 git push origin head
-
+git tag -a "$DATE" -m "Tagging for $DATE deploy"
+git push origin head
+git push origin --tags
